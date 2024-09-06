c7e57d10d7c2d7f4f1a55a9ff8f0127f5926d6eb
x14119641 <daniel.gil.romero24@gmail.com>
2024-09-06 21:56:43 +0200

    just testing with postman some route

--- tutorial_fastapi/main.py
+++ tutorial_fastapi/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
 
 @app.get("/")
-async def read_root():
+async def get_root():
     return {"message": "Hello World!"}
+
+
+@app.get("/posts")
+async def get_posts():
+    return {"message": "Posts data"}
+
+
+@app.post("/createposts")
+async def create_post(payLoad:dict=Body(...)):
+    return {"message": f"Title : {payLoad['title']}, Content: {payLoad['content']}"}
