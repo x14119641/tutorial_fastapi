d3370f05842faa8f07fd0f0cfb7a5ec7ef898df6
x14119641 <daniel.gil.romero24@gmail.com>
2024-09-17 04:50:55 +0200

    Added user and token classes

--- tutorial_fastapi/app/schema.py
+++ tutorial_fastapi/app/schema.py
@@ -1,6 +1,9 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from typing import Optional
 from datetime import datetime
+
+
+
 
 class PostBase(BaseModel):
     title: str
@@ -15,3 +18,34 @@ class PostCreate(PostBase):
 class Post(PostBase):
     id:int
     created_at:datetime
+    
+    
+class UserBase(BaseModel):
+    username: str
+    email:EmailStr
+    disabled: bool | None = None
+    
+    
+class User(UserBase):
+    id:int
+    
+    
+class UserCreate(UserBase):
+    password:str
+
+
+class UserResponse(UserBase):
+    id:int
+    created_at:datetime
+    
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str | None = None
+    
+    
+    
