e44b3c7aa3ccd29d14b98602cb636cac5593096a
x14119641 <daniel.gil.romero24@gmail.com>
2024-09-27 15:31:57 +0200

    Allowed CORS front frontend, perhaps I will need to change something in the origins

--- tutorial_fastapi/app/main.py
+++ tutorial_fastapi/app/main.py
@@ -1,9 +1,23 @@
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routes import hello, user, post, auth, vote
 
 
 app = FastAPI()
+
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials = True,
+    allow_methods=["*"],
+    allow_headers=["*"], 
+)
 
 app.include_router(hello.router)
 app.include_router(auth.router)
